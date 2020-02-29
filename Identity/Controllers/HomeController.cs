fbda10b3bd39f27dbbf4903cfc4fcbd79b0ffd9e
vfarah-if <vincent.farah@codurance.com>
2020-02-29 17:13:53 +0000

    Finialise second simple identity service with in memory manager

---
+++ Identity/Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+
+        public HomeController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Secret()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> LoginAsync(string username, string password)
+        {
+            var user = await userManager.FindByNameAsync(username);
+            if (user != null)
+            {
+                var result = await signInManager.PasswordSignInAsync(user, password, true, false);
+                if (!result.Succeeded)
+                {
+                    return RedirectToAction("Login");
+                }
+            }
+            return RedirectToAction("Index");
+
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RegisterAsync(string username, string password)
+        {
+            var user = new IdentityUser
+            {
+                UserName = username
+            };
+            var result = await userManager.CreateAsync(user, password);
+            if(result.Succeeded)
+            {
+                return await LoginAsync(username, password);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
--- Identity/Startup.cs
+++ Identity/Startup.cs
@@ -1,10 +1,10 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Identity.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,10 +12,36 @@ namespace Identity
 {
     public class Startup
     {
+        private const string CookieAuthenticationName = "CookieAuth";
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // Added to configure in memory database with entity framework
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseInMemoryDatabase("EphemeralDurability");
+            });
+
+
+            // Added to Configure identity
+            services.AddIdentity<IdentityUser, IdentityRole>(options =>
+            {
+                options.Password.RequiredLength = 4; // Configure password options ...
+            })
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders();
+
+            // Added to configure storage to cookie
+            services.ConfigureApplicationCookie(options => 
+            {
+                options.Cookie.Name = "Identity.Cookie";
+                options.LoginPath = "/Home/Login";
+            });
+
+            // Added to utilise views
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +52,22 @@ namespace Identity
                 app.UseDeveloperExceptionPage();
             }
 
+
             app.UseRouting();
+
+            // Added to Authenticate user
+            //who are you?
+            app.UseAuthentication();
+
+            // Added authorise user
+            // What are you allowed to see?
+            app.UseAuthorization();
+
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                // Added to map default controllers
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
